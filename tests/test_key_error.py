7014ba6ed082ac3fd5d611d32a2513b711439d63
André Roberge <andre.roberge@gmail.com>
2022-09-11 22:25:35 -0300

    Adding unit tests

---
+++ tests/test_key_error.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import friendly_traceback as ft
+from friendly_pandas import register
+
+df = pd.DataFrame([[1, 2, 3], [1, 4, 9]],
+                  index=["number", "square"],
+                  columns=["one", "two", "three"])
+
+normal_dict = {"alpha": 1, "beta": 2, "gamma": 3}
+
+
+def test_no_loc_for_col():
+    try:
+        df.loc["one"]
+    except KeyError:
+        ft.explain_traceback(redirect="capture")
+    result = ft.get_output()
+    assert "You tried to use loc to retrieve a column" in result
+
+
+def test_unknown_row():
+    try:
+        df.loc["unknown"]
+    except KeyError:
+        ft.explain_traceback(redirect="capture")
+    result = ft.get_output()
+    assert "You tried to retrieve an unknown row. The valid values are:" in result
+    assert "number, square" in result
+
+
+def test_row_name_typo():
+    try:
+        df.loc["squares"]
+    except KeyError:
+        ft.explain_traceback(redirect="capture")
+    result = ft.get_output()
+    assert "Did you mean `square`" in result
+
+
+def test_normal_dict_key_typo():
+    try:
+        normal_dict["alphas"]
+    except KeyError:
+        ft.explain_traceback(redirect="capture")
+    result = ft.get_output()
+    assert "Did you mean `'alpha'`" in result
