258a5959039dc6ecb780a1c901907d1f94bdc7fa
André Roberge <andre.roberge@gmail.com>
2022-10-18 16:08:27 -0300

    Saving for blog post version.

--- friendly_pandas/specific/setting_with_copy_warning.py
+++ friendly_pandas/specific/setting_with_copy_warning.py
@@ -14,54 +14,37 @@ parser = get_warning_parser(SettingWithCopyWarning)
 @parser.add
 def setting_with_copy_warning(message: str, warning_data: WarningInfo) -> CauseInfo:
     statement = re.sub(r"\s+", "", warning_data.problem_statement)
-    pattern_direct = re.compile(
-        r"""
-        (.*)  # dataframe name
-        \s*\.\s*loc\s*    # .loc  including possible spaces
-        \[(.*)\]       # first index
-        \s*
-        \[(.*)\]       # second index
-        """,
-        re.VERBOSE,
-    )
-    pattern_indirect_1 = re.compile(
-        r"""
-        (.*)  # series name
-        \s*\.\s*loc\s* # . loc including possible spaces
-        \[(.*)\]       # index
-        """,
-        re.VERBOSE,
-    )
+    # Unfortunately, IPython/Jupyter use some temporary files which are not
+    # available after the fact to obtain the statement.
+    if not statement:
+        source = "".join(warning_data.lines)
+        statement = re.sub(r"\s+", "", source)
 
-    pattern_indirect_2 = re.compile(
-        r"""
-        (.*)  # series name
-        \s* #including possible spaces
-        \[(.*)\]       # index
-        """,
-        re.VERBOSE,
-    )
+    pattern_direct = re.compile(r"(.*)\.loc\[(.*)\]\[(.*)\].*")
+    pattern_indirect_1 = re.compile(r"(.*)\.loc\[(.*)\].*")
+    pattern_indirect_2 = re.compile(r"(.*)\[(.*)\].*")
     match = re.match(pattern_direct, statement)
+    match_1 = re.match(pattern_indirect_1, statement)
+    match_2 = re.match(pattern_indirect_2, statement)
     if match:
         return direct_indexing(
             statement, match[1].strip(), match[2].strip(), match[3].strip()
         )
-    match_1 = re.match(pattern_indirect_1, statement)
-    match_2 = re.match(pattern_indirect_2, statement)
-    print(f"{match_1=}")
-    print(f"{match_2=}")
     if match_1:
         series = match_1[1].strip()
         series_obj = get_object_from_name(series, warning_data.frame)
         if isinstance(series_obj, pd.Series):
-            return indirect_indexing(series, match_1[2].strip(), warning_data.frame, loc_1 = ".loc")
+            return indirect_indexing(
+                series, match_1[2].strip(), warning_data.frame, loc_2=".loc"
+            )
     elif match_2:
         series = match_2[1].strip()
         series_obj = get_object_from_name(series, warning_data.frame)
         if isinstance(series_obj, pd.Series):
-            return indirect_indexing(series, match_2[2].strip(), warning_data.frame, loc_2 = ".loc")
+            return indirect_indexing(
+                series, match_2[2].strip(), warning_data.frame, loc_1=".loc"
+            )
     return {}
-
 
 
 def direct_indexing(
@@ -72,9 +55,9 @@ def direct_indexing(
         "of the original content of the dataframe. If you try to assign a value\n"
         "to that copy, the original dataframe will not be modified.\n"
         "Instead of doing a direct chained indexing\n\n"
-        "    {statement}\n\n"
+        "    {dataframe_name}.loc[{index_1}][{index_2}] ...\n\n"
         " try:\n\n"
-        "    {dataframe_name}.loc[{index_1}, {index_2}]\n"
+        "    {dataframe_name}.loc[{index_1}, {index_2}] ...\n"
     ).format(
         statement=statement,
         dataframe_name=dataframe_name,
@@ -92,6 +75,8 @@ def indirect_indexing(
     loc_2: str = "",
 ) -> CauseInfo:
     names = find_all_dataframes(frame)
+    if not names:
+        return {}
     basic_cause = (
         "I suspect that you used indirect chained indexing of a dataframe.\n"
         "First, you likely created a series using something like:\n\n"
@@ -100,26 +85,26 @@ def indirect_indexing(
         "Next, you indexed that copy\n\n"
         "    {series_name}{loc_2}[{index}]\n\n"
         "This had no effect on the original dataframe.\n"
-        "If your goal is to modify the value of the original dataframe\n"
-        "try the following instead:\n\n"
+        "If your goal is to modify the value of the original dataframe,\n"
+        "try something like the following instead:\n\n"
         "    {dataframe_name}.loc[..., {index}]\n"
     )
-    if len(names) == 1:
+    name = names.pop()
+    if len(names) == 0:
         cause = basic_cause.format(
             series_name=series_name,
-            dataframe_name=names.pop(),
+            dataframe_name=name,
             index=index,
             loc_1=loc_1,
             loc_2=loc_2,
         )
     else:
-        name = names.pop()
         names.add(name)
         intro = (
-            "In your code, you have the following dataframes: `{df}`\n"
+            "In your code, you have the following dataframes: `{df}`.\n"
             "I do not know which one is causing the problem here;\n"
             "I will use the name `{name}` as an example.\n\n"
-        ).format(df=names)
+        ).format(df=names, name=name)
         cause = intro + basic_cause.format(
             series_name=series_name,
             dataframe_name=name,
